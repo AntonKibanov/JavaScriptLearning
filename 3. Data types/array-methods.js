6e30976cf398daf944f6f69481cf31c847cad963
Anton Kibanov <a.kibanov@limecreditgroup.com>
2023-03-20 16:31:17 +0700

    Solve tasks with filter method

--- 3. Data types/array-methods.js
+++ 3. Data types/array-methods.js
@@ -4,17 +4,31 @@ function toCamelCase() {
 
     let str2 = camelize(str);
 
-
     function camelize(string) {
         arr = string.split("-");
 
-        arr = arr.map((word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1));
+        arr = arr.map((word, index) =>
+            index == 0 ? word : word[0].toUpperCase() + word.slice(1)
+        );
 
         str = arr.join("");
 
         return str;
     }
 
-    alert (str2);
+    alert(str2);
+}
 
+//Второе задание
+
+function filterArray() {
+    let myArr = [5, 3, 8, 1];
+
+    let filteredArray = filterRange(myArr, 1, 4);
+
+    function filterRange(arr, a, b) {
+        return arr.filter(item => (item >= a && item <= b));
+    }
+
+    alert(filteredArray);
 }
